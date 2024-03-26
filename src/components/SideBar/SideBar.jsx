1649a8335e448e604b8310541c0c91d2940435a1
IgorMarines <igor.marines25@gmail.com>
2024-03-25 22:17:12 -0300

    feature: :sparkle: menu-burger

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,9 +4,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import house from '../../images/house.png';
 import star from '../../images/star.png';
+import menu_open from '../../images/menu-burger-open.svg';
+import menu_close from '../../images/menu-burger-close.svg';
+import logout from '../../images/logout.png';
+import {palette} from '../../shared/colors/palette'
 
 const SideBar = ({ navigation }) => {
   const [isDarkMode, setIsDarkMode] = useState(false); // Estado para controlar o tema escuro
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Estado para controlar a abertura do menu lateral
 
   const navigateToHome = () => {
     navigation.navigate('Home');
@@ -37,43 +42,56 @@ const SideBar = ({ navigation }) => {
     }
   };
 
+
   return (
     <ScrollView
       horizontal // Adiciona scroll horizontal
       contentContainerStyle={styles.container}
+      style={{ backgroundColor: palette.blue_primary, width: '100%' }}
     >
-      <TouchableOpacity style={styles.button} onPress={navigateToHome}>
-        <Image style={{ width: 15, height: 15, marginRight: 10 }} source={house} alt="Ícone da página de home" />
-        <Text style={styles.buttonText}>Home</Text>
+      <TouchableOpacity onPress={() => setIsSidebarOpen(!isSidebarOpen)} style={{paddingLeft: 20}}>
+        <Text>
+          <Image style={{ width: 25, height: 25, marginRight: 10 }} source={isSidebarOpen ? menu_close : menu_open} />
+        </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={navigateToFavorites}>
-        <Image style={{ width: 15, height: 15, marginRight: 10 }} source={star} alt="Ícone da página de favoritos" />
-        <Text style={styles.buttonText}>Favorites</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleLogout}>
-        <Text style={styles.buttonText}>Logout</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={setDarkMode}>
-        <Text style={styles.buttonText}>Dark Theme</Text>
-      </TouchableOpacity>
+      {
+        isSidebarOpen && (
+          <>
+            <TouchableOpacity style={styles.button} onPress={navigateToHome}>
+              <Image style={{ width: 15, height: 15, marginRight: 10 }} source={house} alt="Ícone da página de home" />
+              <Text style={styles.buttonText}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={navigateToFavorites}>
+              <Image style={{ width: 15, height: 15, marginRight: 10 }} source={star} alt="Ícone da página de favoritos" />
+              <Text style={styles.buttonText}>Favorites</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleLogout}>
+              <Image style={{ width: 15, height: 15, marginRight: 10 }} source={logout} alt="Ícone da página de logout" />
+              <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+          </>
+        )
+      }
+
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#5458FD',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingVertical: 10,
+    width: '80%'
   },
   button: {
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
     borderColor: 'white', // Cor da borda
-    backgroundColor: '#4F46E5',
+    backgroundColor: 'white',
+    
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 10,
@@ -81,7 +99,7 @@ const styles = StyleSheet.create({
     marginRight: 5,
   },
   buttonText: {
-    color: '#fff',
+    color: palette.blue_primary,
     fontSize: 16,
     fontWeight: 'bold',
   },
